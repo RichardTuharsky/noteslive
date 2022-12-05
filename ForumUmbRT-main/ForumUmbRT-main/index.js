90bb64bf156c9536c5e060267a9f37538477b412
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-12-05 16:23:16 +0100

    .

--- ForumUmbRT-main/ForumUmbRT-main/index.js
+++ ForumUmbRT-main/ForumUmbRT-main/index.js
@@ -4,16 +4,23 @@ var myActivities = [];
 
 document.querySelector('form button').addEventListener('click', function(event) {
 
+  const date = new Date();
   var inputs = document.querySelectorAll('form input');
   var newActivity = {};
   for (var i = 0; i < inputs.length; i++) {
     newActivity[inputs[i].name] = inputs[i].value;
     inputs[i].value = '';
+    
   }
+  
   myActivities.push(newActivity);
   console.log(myActivities);
+  console.log(date.toLocaleString());
   event.preventDefault();
-  document.getElementById("posts").innerHTML = myActivities;
+
+  document.getElementById("posts").innerHTML += myActivities.join(", ");
+
+  //myActivities.sort();
 
 },false);
 
