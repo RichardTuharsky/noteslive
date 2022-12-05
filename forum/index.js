b1e3133bed19d414a653e2f557a087c9f3fad30d
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-12-05 09:54:26 +0100

    ..

---
+++ forum/index.js
@@ -0,0 +1,15 @@
+var myActivities = [];
+
+document.querySelector('form button').addEventListener('click', function(event) {
+
+  var inputs = document.querySelectorAll('form input');
+  var newActivity = {};
+  for (var i = 0; i < inputs.length; i++) {
+    newActivity[inputs[i].name] = inputs[i].value;
+    inputs[i].value = '';
+  }
+  myActivities.push(newActivity);
+  console.log(myActivities);
+  event.preventDefault();
+
+}, false);
